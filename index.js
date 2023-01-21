1b1873b45c91896d51f30b9a9509586da4cf191d
i-am-riya <76762468+i-am-riya@users.noreply.github.com>
2023-01-21 16:40:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,18 +4,31 @@ let count = 0
 const increment = () => {
      count++;
     value.innerHTML = count;
+    if(count > 0){
+  document.getElementById("value").style.color = "green";
+}
+  if(count === 0){
+  document.getElementById("value").style.color = "black";
+}
 }
 
 const decrement = () => {
-    if(count > 0){
         count--;
         value.innerHTML = count;
-    }
+        if(count < 0){
+  document.getElementById("value").style.color = "red";
+}
+  if(count === 0){
+  document.getElementById("value").style.color = "black";
+}
 }
 
 const reset = () => {
     if(count !== 0){
         count = 0;
         value.innerHTML = count;
+  if(count === 0){
+  document.getElementById("value").style.color = "black";
+}
     }
 }
